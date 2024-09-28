696f8b5fe55482b0a746257f41f9673430020793
random776 <125573400+random776@users.noreply.github.com>
2024-09-29 02:03:22 +0900

    change icon

--- src/app/chibutsu/cgs/page.tsx
+++ src/app/chibutsu/cgs/page.tsx
@@ -7,29 +7,32 @@ import "../../styles/style.css";
 import "../../styles/card.css";
 import "../../styles/notice.css";
 import "../../styles/news.css";
-import 'katex/dist/katex.min.css';
+import "katex/dist/katex.min.css";
 
 // import MathRenderer from '../../Basic/MathRenderer'
 import Markdown from "react-markdown";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 const markdown = `
- 準備中です。
-`;
+ > CGS静電単位系は、クーロンの法則を元にして、クーロンの法則が係数を含まなくなるように電荷の単位を決定し、電荷の単位を元に電流などの他の電磁気の単位を構築したものである。CGS静電単位系では、クーロンの法則は次のように書き表される。 
+ $$ 
+     F = \\frac{q_1 q_2}{r^2}
+   $$
 
+`;
 
 export default function Result() {
   return (
     <>
       <Header2 />
       <div className="sonota">
-        <h2 className="color">CGSガウス単位系に関する備忘録</h2>
+        <h2 className="color">CGSガウス単位系に関するリンク集</h2>
         <p>最終更新：2024年9月18日</p>
         <div className="section_box">
-          以下の文章は地球惑星物理・環境学科の五月祭2024(5/18,19)で実際に展示した文章です。なお、そこまで読んでくださった方は多くなかった模様。
+          『宇宙電磁流体力学の基礎』でほとんどCGSガウス単位系を説明せずに使っていたので、その備忘録として記しておく。
         </div>
         <div className="content">
-        <Markdown rehypePlugins={[rehypeKatex]} remarkPlugins={[remarkMath]}>
+          <Markdown rehypePlugins={[rehypeKatex]} remarkPlugins={[remarkMath]}>
             {markdown}
           </Markdown>
         </div>
